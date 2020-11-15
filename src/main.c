4a205e1530b953f775c35fec598289feaef7b1d4
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2020-11-15 18:41:37 +0100

    Read content of file provided on commandline

--- src/main.c
+++ src/main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "./codeGenerator.h"
 
-#define COMPILER_STATEMENT "echo \'%s\' | gcc -o generated/bf -xc -w -"
-
-char* bf =
-    "++++++++++[ > +++++++ > ++++++++++ > +++ > + < < < < -] Schleife zur Vorbereitung der Textausgabe >++.Ausgabe von "
-    "'H' > +.Ausgabe von 'e' +++++++.'l'.'l' +++.'o' >++.Leerzeichen << +++++++++++++++.'W' >.'o' +++.'r' ------.'l' "
-    "--------.'d' >+.'!' >.Zeilenvorschub ";
+#define COMPILER_STATEMENT "echo \'%s\' | gcc -o %s -xc -w -"
 
 /**
  * Write a string to a file.
@@ -20,10 +16,40 @@ void writeToFile(const char* value, const char* filename) {
     fclose(file);
 }
 
-int main() {
+/**
+ * Read content from a file.
+ */
+char* readFromFile(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        printf("Cannot open file '%s'\n", filename);
+        exit(1);
+    }
+
+    char* content = malloc(sizeof(""));
+    char c;
+
+    while ((c = fgetc(file)) != EOF) {
+        content = realloc(content, strlen(content) + 1);
+        content[strlen(content)] = c;
+    }
+    fclose(file);
+    return content;
+}
+
+int main(const int argc, const char** argv) {
+    if (argc < 3) {
+        printf("Not enough arguments!\nUsage: %s <input> <output>\n", argv[0]);
+        return 1;
+    }
+
+    const char* inputFile = argv[1];
+    const char* outputFile = argv[2];
+
+    const char* bf = readFromFile(inputFile);
+
     char* code = generateCodeFromBf(bf);
-    writeToFile(code, "./generated/bf.c");
-    char* output = malloc(strlen(code) + strlen(COMPILER_STATEMENT) + 1);
-    sprintf(output, COMPILER_STATEMENT, code);
+    char* output = malloc(strlen(code) + strlen(outputFile) + strlen(COMPILER_STATEMENT) + 1);
+    sprintf(output, COMPILER_STATEMENT, code, outputFile);
     system(output);
 }
